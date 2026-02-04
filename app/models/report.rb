1d6748aa9b695ee10fc44f066f1ba0572e10098a
Swarmz <gavinsutton6511@gmail.com>
2026-02-04 22:39:38 +0900

    refactor association names to be more readable

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,15 +3,15 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
-  after_save :sync_made_mentions
+  after_save :sync_sent_mentions
 
   has_many :received_mentions, class_name: 'ReportMention', foreign_key: :mentioned_report_id, inverse_of: :mentioned_report, dependent: :destroy
   # reports that mention this report
   has_many :mentioned_reports, through: :received_mentions, source: :mentioning_report
 
-  has_many :made_mentions, class_name: 'ReportMention', foreign_key: :mentioning_report_id, inverse_of: :mentioning_report, dependent: :destroy
+  has_many :sent_mentions, class_name: 'ReportMention', foreign_key: :mentioning_report_id, inverse_of: :mentioning_report, dependent: :destroy
   # reports this report mentions
-  has_many :mentioning_reports, through: :made_mentions, source: :mentioned_report
+  has_many :mentioning_reports, through: :sent_mentions, source: :mentioned_report
 
   validates :title, presence: true
   validates :content, presence: true
@@ -26,7 +26,7 @@ class Report < ApplicationRecord
 
   private
 
-  def sync_made_mentions
+  def sync_sent_mentions
     mentioned_report_ids = content.to_s.scan(%r{/reports/(\d+)}).flatten.map(&:to_i).uniq
     self.mentioning_report_ids = mentioned_report_ids - [id] # prevent self-reference
   end
--- app/models/report_mention.rb
+++ app/models/report_mention.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ReportMention < ApplicationRecord
-  belongs_to :mentioning_report, class_name: 'Report', inverse_of: :made_mentions
+  belongs_to :mentioning_report, class_name: 'Report', inverse_of: :sent_mentions
   belongs_to :mentioned_report, class_name: 'Report', inverse_of: :received_mentions
 
   validates :mentioned_report_id, uniqueness: { scope: :mentioning_report_id }
